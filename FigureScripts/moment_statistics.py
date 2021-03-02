5957c75de99457164c01ee8a168a6164ec1b7852
crheller <charlieheller95@gmail.com>
2021-03-01 21:00:15 -0800

    figuring out decay rules

--- FigureScripts/moment_statistics.py
+++ FigureScripts/moment_statistics.py
@@ -9,55 +9,6 @@ import matplotlib as mpl
 mpl.rcParams['axes.spines.right'] = False
 mpl.rcParams['axes.spines.top'] = False
 
-N = 10
-size = np.arange(N, 1000, 10)
-u = np.random.normal(0, 1, N) # True means
-lv = np.abs(np.random.normal(0, 1, (N, 1)))
-lv /= np.linalg.norm(lv)
-cov = lv.dot(lv.T)
-cov += np.random.normal(0, 0.1, cov.shape) # true covariance
-merr = []
-varerr = []
-pc1 = []
-for n in size:
-    print(f"n {n}")
-    m = []
-    v = []
-    p = []
-    for i in range(0, 100):
-        vals = np.random.multivariate_normal(u, cov, n)
-        m.append(u - vals.mean(axis=0))
-        v.append(cov.flatten() - np.cov(vals.T).flatten())
-        _, e = np.linalg.eig(np.cov(vals.T))
-        p.append((e[:, 0].dot(lv)[0]))
-    merr.append(np.stack(np.abs(m)).mean(axis=0))
-    varerr.append(np.stack(np.abs(v)).mean(axis=0))
-    pc1.append(np.mean(np.abs(p)))
-
-merr = np.stack(merr)
-varerr = np.stack(varerr)
-
-f, ax = plt.subplots(1, 1, figsize=(5, 5))
-
-m = (merr / abs(u)).mean(axis=-1)
-sd = (merr / abs(u)).std(axis=-1)
-ax.plot(size, m)
-ax.fill_between(size, m-sd, m+sd, alpha=0.5, lw=0)
-m = (varerr / abs(cov.flatten())).mean(axis=-1)
-sd = (varerr / abs(cov.flatten())).std(axis=-1)
-ax.plot(size, m)
-ax.fill_between(size, m-sd, m+sd, alpha=0.5, lw=0)
-ax.plot(size, pc1)
-#ax.set_ylim((0, 1))
-ax.set_ylabel("Percentage error")
-ax.axvline(N, linestyle='--', color='k')
-
-ax.set_title(f"Cov: {cov[0,1]:.4f}, Mean: {u[0]:.4f}")
-
-f.tight_layout()
-
-plt.show()
-
 # one-D case
 nsamps = 1000
 u = 5
@@ -67,7 +18,8 @@ var = []
 var_var = []
 mean_var = []
 n = np.arange(2, 100, 1)
-for ss in n:
+for ii, ss in enumerate(n):
+    print(f"Sample {ii} /{len(n)}")
     _mean = []
     _variance = []
     for i in range(0, nsamps):
@@ -83,6 +35,8 @@ f, ax = plt.subplots(2, 2, figsize=(6, 6))
 
 ax[0, 0].plot(n, mean_var, label=f'mean: {u}')
 ax[0, 0].plot(n, var_var, label=f'variance: {(sd**2):.3f}')
+ax[0, 0].plot(n, (sd**2)/n + ((sd**2) / nsamps), lw=5, color='tab:blue', alpha=0.5, zorder=-1, label=r'$\sigma^2$/$n$')
+ax[0, 0].plot(n, ((sd**2)**2 / (0.5*(n))), lw=5, color='tab:orange', alpha=0.5, zorder=-1, label=r'$2(\sigma^2)^2$/$(n)$')
 ax[0, 0].set_ylabel(f'Var. of param estimate\nacross {nsamps} samples')
 ax[0, 0].set_xlabel('Sample size')
 ax[0, 0].legend(frameon=False)
@@ -92,11 +46,11 @@ ax[0, 1].set_ylabel('Ratio')
 ax[0, 1].set_xlabel('Sample size')
 
 # ratio of variance in estimate to true param value
-ax[1, 0].plot(n, np.array(mean_var)/mean, color='tab:blue')
+ax[1, 0].plot(n, np.array(mean_var) / u, color='tab:blue')
 ax[1, 0].set_ylabel('var(mean) / mean')
 ax[1, 0].set_xlabel('Sample size')
 
-ax[1, 1].plot(n, np.array(var_var)/(sd**2), color='tab:orange')
+ax[1, 1].plot(n, np.array(var_var) / (sd**2), color='tab:orange')
 ax[1, 1].set_ylabel('var(var) / var')
 ax[1, 1].set_xlabel('Sample size')
 
@@ -107,14 +61,15 @@ plt.show()
 
 # two-D case -- do covariance values behave like variance?
 nsamps = 1000
-u = [5, 5]
-cov = np.array([[2, 0.4], [0.4, 2]])
+u = [10, 4]
+cov = np.array([[4, 0.9], [0.9, 1]])
 mean = []
 var = []
 var_var = []
 mean_var = []
 n = np.arange(2, 100, 1)
-for ss in n:
+for ii, ss in enumerate(n):
+    print(f"Sample {ii} /{len(n)}")
     _mean = []
     _variance = []
     for i in range(0, nsamps):
@@ -126,26 +81,56 @@ for ss in n:
     var_var.append(np.stack(_variance).var(axis=0))
     mean_var.append(np.stack(_mean).var(axis=0))
 
-f, ax = plt.subplots(2, 2, figsize=(6, 6))
+mean = np.stack(mean)
+var = np.stack(var)
+var_var = np.stack(var_var)
+mean_var = np.stack(mean_var)
 
-ax[0, 0].plot(n, mean_var, label=f'mean: {u}')
-ax[0, 0].plot(n, var_var, label=f'variance: {(sd**2):.3f}')
+f, ax = plt.subplots(2, 3, figsize=(13, 8))
+
+# mean
+ax[0, 0].plot(n, mean_var[:, 0], label=f'mean[0]: {u[0]}')
+ax[0, 0].plot(n, mean_var[:, 1], label=f'mean[1]: {u[1]}')
+# variance
+ax[0, 0].plot(n, var_var[:, 0, 0], label=f'variance[0, 0]: {(cov[0, 0]):.3f}')
+ax[0, 0].plot(n, ((cov[0, 0])**2 / (0.5*(n-1))), lw=5, color='tab:green', alpha=0.5, zorder=-1, label=r'$2(\sigma^2)^2$/$(n-1)$')
+ax[0, 0].plot(n, var_var[:, 1, 1], label=f'variance[1, 1]: {(cov[1, 1]):.3f}')
+# covariance
+ax[0, 0].plot(n, var_var[:, 0, 1], label=f'covariance[0, 1]: {(cov[0, 1]):.3f}')
+ax[0, 0].plot(n, ((cov[0, 0] * cov[1, 1]) / (n-1)), lw=5, color='tab:purple', alpha=0.5, zorder=-1, label=r'$(\sigma_0^2\sigma_1^2)$/$(n-1)$')
+ax[0, 0].plot(n, var_var[:, 1, 0], label=f'covariance[1, 0]: {(cov[1, 0]):.3f}')
 ax[0, 0].set_ylabel(f'Var. of param estimate\nacross {nsamps} samples')
 ax[0, 0].set_xlabel('Sample size')
 ax[0, 0].legend(frameon=False)
 
-ax[0, 1].plot(n, np.array(var_var) / np.array(mean_var))
+ax[0, 1].plot(n, var_var[:, 0, 0] / mean_var[:, 0], label='cov[0, 0]:mean[0]')
+ax[0, 1].plot(n, var_var[:, 0, 1] / mean_var[:, 0], label='cov[0, 1]:mean[0]')
+ax[0, 1].plot(n, var_var[:, 0, 1] / mean_var[:, 1], label='cov[0, 1]:mean[1]')
 ax[0, 1].set_ylabel('Ratio')
 ax[0, 1].set_xlabel('Sample size')
+ax[0, 1].legend(frameon=False)
 
 # ratio of variance in estimate to true param value
-ax[1, 0].plot(n, np.array(mean_var)/mean, color='tab:blue')
+ax[1, 0].plot(n, mean_var[:, 0] / u[0], color='tab:blue', label='mean[0]')
 ax[1, 0].set_ylabel('var(mean) / mean')
 ax[1, 0].set_xlabel('Sample size')
+ax[1, 0].legend(frameon=False)
 
-ax[1, 1].plot(n, np.array(var_var)/(sd**2), color='tab:orange')
+ax[1, 1].plot(n, var_var[:, 0, 0] / cov[0, 0], color='tab:green', label='cov[0, 0]')
+ax[1, 1].plot(n, var_var[:, 0, 1] / cov[0, 1], color='tab:purple', label='cov[0, 1]')
 ax[1, 1].set_ylabel('var(var) / var')
 ax[1, 1].set_xlabel('Sample size')
+ax[1, 1].legend(frameon=False)
+
+# ratio of % errs
+ax[1, 2].plot(n, (mean_var[:, 0] / u[0]) / (var_var[:, 0, 0] / cov[0, 0]), label='mean[0]:cov[0, 0]')
+ax[1, 2].plot(n, (mean_var[:, 0] / u[0]) / (var_var[:, 0, 1] / cov[0, 1]), label='mean[0]:cov[0, 1]')
+ax[1, 2].plot(n, (var_var[:, 0, 0] / cov[0, 0]) / (var_var[:, 0, 1] / cov[0, 1]), label='cov[0, 0]:cov[0, 1]')
+
+ax[1, 2].legend(frameon=False, bbox_to_anchor=(1, 1), loc='upper left')
+
+ax[1, 2].set_xlabel('Sample size')
+ax[1, 2].set_ylabel('Ratio')
 
 f.tight_layout()
 
