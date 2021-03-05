6fcb97a29624cdd696c9a37963006a2fdcf11cfe
crheller <charlieheller95@gmail.com>
2021-03-04 17:30:24 -0800

    polishing figure 1

---
+++ FigureScripts/fig1.py
@@ -0,0 +1,160 @@
+"""
+Simple two-neuron schematic of single trial responses and decoding axis for high / low rep conditions
+
+Shuffled distribution of pairwise covariance for the two neurons in each conditions
+
+Covariance matrix
+
+cross-validated d'^2 vs. k and var(covariance) vs. k. On same axes.
+"""
+from utils.decoding import compute_dprime
+from utils.plotting import compute_ellipse
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib as mpl
+mpl.rcParams['axes.spines.right'] = False
+mpl.rcParams['axes.spines.top'] = False
+mpl.rcParams['font.size'] = 8
+
+np.random.seed(123)
+
+savefig = True
+fig_name = os.path.join(os.getcwd(), 'figures/fig1.svg')
+
+# generate pseudo data, X, for decoding
+k1 = 25
+k2 = 250
+u1 = [2, 0]
+u2 = [-2, 0]
+cov = np.array([[2, 0.2], [0.2, 2]])
+x11 = np.random.multivariate_normal(u1, cov, k1)
+x21 = np.random.multivariate_normal(u2, cov, k1)
+X1 = np.stack([x11, x21])
+x12 = np.random.multivariate_normal(u1, cov, k2)
+x22 = np.random.multivariate_normal(u2, cov, k2)
+X2 = np.stack([x12, x22])
+
+# get decoding axis and projections
+d = compute_dprime(x11.T, x21.T)
+wopt1 = d.wopt * 2
+d = compute_dprime(x12.T, x22.T)
+wopt2 = d.wopt * 2
+
+# generate list of shuffled covariance values for k=k1/k2
+nshuff = 500
+scov1 = []
+scov2 = []
+for i in range(0, nshuff):
+    x = np.random.choice(x11[:, 0], x11.shape[0], replace=False)
+    y = np.random.choice(x11[:, 1], x11.shape[0], replace=False)
+    scov1.append(np.cov(x, y)[0, 1])
+    x = np.random.choice(x12[:, 0], x12.shape[0], replace=False)
+    y = np.random.choice(x12[:, 1], x12.shape[0], replace=False)
+    scov2.append(np.cov(x, y)[0, 1])
+
+# for a range of k, generate shuffled distros
+kscov = []
+dp = []
+krange = np.arange(10, 400, 2)
+_x1 = np.random.multivariate_normal(u1, cov, 1000)
+for i, _k in enumerate(krange):
+    _s = []
+    _dp = []
+    print(f"{int((i/len(krange)) * 100)} percent finished generating shuffles")
+    for i in range(0, nshuff):
+        x = np.random.choice(_x1[:, 0], _k, replace=False)
+        y = np.random.choice(_x1[:, 1], _k, replace=False)
+        _s.append(np.cov(x, y)[0, 1])
+        r1 = np.random.multivariate_normal(u1, cov, _k)
+        r2 = np.random.multivariate_normal(u2, cov, _k)
+        # get cv dprime w/ simple 50/50 split of data
+        est = np.random.choice(np.arange(_k), int(_k/2), replace=False)
+        val = np.array(list(set(np.arange(_k)).difference(set(est))))
+        # train decoder
+        resf = compute_dprime(r1[est, :].T, r2[est, :].T)
+        rest = compute_dprime(r1[val, :].T, r2[val, :].T, wopt=resf.wopt)
+        _dp.append(abs(rest.dprimeSquared - resf.dprimeSquared))
+    dp.append(_dp)
+    kscov.append(_s)
+dp = np.stack(dp)
+kscov = np.stack(kscov)
+
+# plot the results
+f, ax = plt.subplots(2, 3, figsize=(6, 4))
+
+# plot decoding figure1
+ax[0, 0].scatter(X1[0, :, 0], X1[0, :, 1], s=3, alpha=0.5, lw=0)
+ax[0, 0].scatter(X1[1, :, 0], X1[1, :, 1], s=3, alpha=0.5, lw=0)
+x, y = compute_ellipse(X1[0, :, 0], X1[0, :, 1])
+ax[0, 0].plot(x, y, color='tab:blue', label='Stim. a')
+x, y = compute_ellipse(X1[1, :, 0], X1[1, :, 1])
+ax[0, 0].plot(x, y, color='tab:orange', label='Stim. b')
+# plot decoding axis
+ref = np.array(u1) - ((np.array(u1) - np.array(u2)) / 2)
+ax[0, 0].plot([ref[0]-wopt1[0], ref[0]+wopt1[0]], 
+            [ref[1]-wopt1[1], ref[1]+wopt1[1]], 'k-')
+ax[0, 0].legend(frameon=False)
+ax[0, 0].set_xlabel(r"$n_1$ spike counts")
+ax[0, 0].set_ylabel(r"$n_2$ spike counts")
+ax[0, 0].axis('equal')
+ax[0, 0].set_title(r"$k=%s$"%str(k1))
+
+# plot decoding figure2
+ax[0, 1].scatter(X2[0, :, 0], X2[0, :, 1], s=3, alpha=0.5, lw=0)
+ax[0, 1].scatter(X2[1, :, 0], X2[1, :, 1], s=3, alpha=0.5, lw=0)
+x, y = compute_ellipse(X2[0, :, 0], X2[0, :, 1])
+ax[0, 1].plot(x, y, color='tab:blue', label='Stim. a')
+x, y = compute_ellipse(X2[1, :, 0], X2[1, :, 1])
+ax[0, 1].plot(x, y, color='tab:orange', label='Stim. b')
+# plot decoding axis
+ref = np.array(u1) - ((np.array(u1) - np.array(u2)) / 2)
+ax[0, 1].plot([ref[0]-wopt2[0], ref[0]+wopt2[0]], 
+            [ref[1]-wopt2[1], ref[1]+wopt2[1]], 'k-')
+ax[0, 1].legend(frameon=False)
+ax[0, 1].set_xlabel(r"$n_1$ spike counts")
+ax[0, 1].set_ylabel(r"$n_2$ spike counts")
+ax[0, 1].axis('equal')
+ax[0, 1].set_title(r"$k=%s$"%str(k2))
+
+# plot covariance matrix
+sns.heatmap(cov, cmap='Reds', annot=True, ax=ax[0, 2], 
+                xticklabels=[r"$n_1$", r"$n_2$"], yticklabels=[r"$n_1$", r"$n_2$"], cbar=False)
+ax[0, 2].set_title(r"$\Sigma=\Sigma_a=\Sigma_b$")
+
+# plot shuffled distro for k=k in decoding figure
+llim, hlim = (-2, 2)
+ax[1, 0].hist(scov1, bins=np.arange(llim, hlim, 0.1), histtype='stepfilled', edgecolor='k', alpha=0.5)
+ax[1, 0].axvline(cov[0, 1], color='r')
+ax[1, 0].set_xlim((llim, hlim))
+ax[1, 0].set_title(r"$k=%s, \sigma^2=%s$" % (str(k1), str(round(np.var(scov1), 3))))
+ax[1, 0].set_xlabel(r"$cov(n_1, n_2)$ after shuffling")
+
+ax[1, 1].hist(scov2, bins=np.arange(llim, hlim, 0.1), histtype='stepfilled', edgecolor='k', alpha=0.5)
+ax[1, 1].axvline(cov[0, 1], color='r')
+ax[1, 1].set_xlim((llim, hlim))
+ax[1, 1].set_title(r"$k=%s, \sigma^2=%s$" % (str(k2), str(round(np.var(scov2), 3))))
+ax[1, 1].set_xlabel(r"$cov(n_1, n_2)$ after shuffling")
+
+# plot range of shuffle var for different values of k
+ax[1, 2].plot(krange, kscov.var(axis=-1))
+ax[1, 2].axhline(0, linestyle='--', color='k')
+ax[1, 2].set_ylabel(r"$\sigma^2$ of shuff. distribution", color='tab:blue')
+ax[1, 2].set_xlabel(r"No. of samples ($k$)")
+ax[1, 2].tick_params(axis='y', labelcolor='tab:blue')
+
+# generate twin axes for d'^2
+ax2 = ax[1, 2].twinx()
+ax2.spines['right'].set_visible(True)
+ax2.plot(krange, dp.mean(axis=-1), color='orange')
+ax2.set_ylabel(r"Error in $d'^2$ estimate"+"\nabs(Est. - Val.)", color='orange')
+ax2.tick_params(axis='y', labelcolor='orange')
+
+f.tight_layout()
+
+if savefig:
+    f.savefig(fig_name)
+
+plt.show()
---
+++ utils/decoding.py
@@ -0,0 +1,187 @@
+"""
+Functions for computing d-prime from a pair of response distributions
+"""
+from collections import namedtuple
+import numpy as np
+import logging
+
+log = logging.getLogger()
+
+def compute_dprime(A, B, diag=False, wopt=None):
+    """
+    Compute discriminability between matrix A and matrix B
+    where both are shape N neurons X N reps.
+
+    A / B: N x k numpy arrays
+    diag: bool, if True, compute dprime assuming diagonal covariance matrix
+    wopt: N x 1 numpy array -- if not None, use this axis as the decoding axis 
+
+    Return:
+        dprime ** 2 (with sign preserved... so that we can evaluate consitency of sign btwn est/val sets)
+        decoding axis (with norm 1)
+        evals: (of mean covariance matrix)
+        evecs: (of mean covariance matrix)
+        evec_sim: similarity of first eigenvector between the two stimuli, A and B
+        dU: <A> - <B>
+    """
+    if (A.shape[0] > A.shape[1]) & (wopt is None):
+        raise ValueError("Number of dimensions greater than number of observations. Unstable")
+
+    if A.shape[0] != B.shape[0]:
+        raise ValueError("Number of dimensions do not match between conditions")
+
+    if A.shape[0] == 1:
+        # single dimension
+        dprime = _dprime_single_dim(A, B)
+        return dprime
+
+    if diag:
+        dprime, wopt, evals, evecs, evec_sim, dU = _dprime_diag(A, B)
+
+    else:
+        dprime, wopt, evals, evecs, evec_sim, dU = _dprime(A, B, wopt=wopt)
+
+    dprime_results = {
+        'dprimeSquared': dprime,
+        'wopt': wopt,
+        'evals': evals,
+        'evecs': evecs,
+        'evecSim': evec_sim,
+        'dU': dU
+    }
+
+    return namedtuple('DecodingResults', dprime_results.keys())(**dprime_results)
+
+
+def _dprime_single_dim(A, B):
+    """
+    Simplest case of single neuron (dimension) dprime -- classic signal detection theory
+    u1 - u2 / sqrt(0.5 * (sig1 + sig2))
+    """
+    num = A.mean() - B.mean()
+    den = np.sqrt(0.5 * (A.var() + B.var()))
+    if num == 0:
+        return 0
+    elif den == 0:
+        return np.inf
+    else:
+        return num / den
+
+
+def _dprime(A, B, wopt=None):
+    """
+    See Rumyantsev et. al 2020, Nature for nice derivation
+    """
+
+    sigA = np.cov((A - A.mean(axis=-1, keepdims=True)))
+    sigB = np.cov((B - B.mean(axis=-1, keepdims=True)))
+
+    usig = 0.5 * (sigA + sigB)
+    u_vec = (A.mean(axis=-1) - B.mean(axis=-1))[np.newaxis, :]    
+
+    try:
+        valA, vecA = np.linalg.eig(sigA)
+        valB, vecB = np.linalg.eig(sigB)
+        evec_sim = abs(vecB[:, np.argsort(valB)[::-1][0]].dot(vecA[:, np.argsort(valA)[::-1][0]]))
+    except:
+        evec_sim = np.nan
+
+    if wopt is not None:
+        wopt_train = wopt / np.linalg.norm(wopt)
+        A = A.T.dot(wopt_train).T
+        B = B.T.dot(wopt_train).T
+
+        usig_ = 0.5 * (np.cov((A - A.mean(axis=-1, keepdims=True))) + np.cov((B - B.mean(axis=-1, keepdims=True))))
+        u_vec_ = (A.mean(axis=-1) - B.mean(axis=-1))[np.newaxis, :]
+    
+    try:
+        if wopt is not None:
+            wopt = (1 / usig_) * u_vec_
+            dp2 = np.matmul(u_vec_, wopt)[0][0]
+            try:
+                # if wopt is passed, could still compute dpirme but can't compute 
+                # evecs/ evals
+                evals, evecs = np.linalg.eig(usig)
+                # make sure evals / evecs are sorted
+                idx_sort = np.argsort(evals)[::-1]
+                evals = evals[idx_sort]
+                evecs = evecs[:, idx_sort]
+            except:
+                wopt = np.nan * np.ones((A.shape[0], 1))
+                evals = np.nan * np.ones((A.shape[0], ))
+                evecs = np.nan * np.ones((A.shape[0], A.shape[0]))
+
+        else:
+            inv = np.linalg.inv(usig)
+            wopt = inv @ u_vec.T
+            dp2 = np.matmul(u_vec, wopt)[0][0]
+
+            evals, evecs = np.linalg.eig(usig)
+            # make sure evals / evecs are sorted
+            idx_sort = np.argsort(evals)[::-1]
+            evals = evals[idx_sort]
+            evecs = evecs[:, idx_sort]
+
+    except:
+        log.info('WARNING, Singular Covariance, dprime infinite, set to np.nan')
+        wopt_nan = np.nan * np.ones((A.shape[0], 1))
+        evals_nan = np.nan * np.ones((A.shape[0], ))
+        evecs_nan = np.nan * np.ones((A.shape[0], A.shape[0]))
+        u_vec_nan =  np.nan * np.ones((1, A.shape[0]))
+        return np.nan, wopt_nan, evals_nan, evecs_nan, np.nan, u_vec_nan
+
+    return dp2, wopt, evals, evecs, evec_sim, u_vec
+
+
+def _dprime_diag(A, B):
+    """
+    See Rumyantsev et. al 2020, Nature  and Averbeck 2006, JNP for nice derivations.
+        Note typo in Rumyantsev though!
+    """
+
+    sigA = np.cov((A - A.mean(axis=-1, keepdims=True)))
+    sigB = np.cov((B - B.mean(axis=-1, keepdims=True)))
+
+    usig = 0.5 * (sigA + sigB)
+    u_vec = (A.mean(axis=-1) - B.mean(axis=-1))[np.newaxis, :]    
+
+    try:
+        valA, vecA = np.linalg.eig(sigA)
+        valB, vecB = np.linalg.eig(sigB)
+        evec_sim = abs(vecB[:, np.argsort(valB)[::-1][0]].dot(vecA[:, np.argsort(valA)[::-1][0]]))
+    except:
+        evec_sim = np.nan
+
+    u_vec = (A.mean(axis=-1) - B.mean(axis=-1))[np.newaxis, :]
+
+    try:
+        # get diagonal covariance matrix
+        usig_diag = np.zeros(usig.shape)
+        np.fill_diagonal(usig_diag, np.diagonal(usig))
+
+        # compute numerator
+        numerator = (u_vec @ np.linalg.inv(usig_diag) @ u_vec.T) ** 2
+
+        # compute denominator
+        denominator = u_vec @ np.linalg.inv(usig_diag) @ usig @ np.linalg.inv(usig_diag) @ u_vec.T
+        denominator = denominator[0][0]
+    except np.linalg.LinAlgError:
+        log.info('WARNING, Singular Covariance, dprime infinite, set to np.nan')
+        wopt_nan = np.nan * np.ones((A.shape[0], 1))
+        evals_nan = np.nan * np.ones((A.shape[0], ))
+        evecs_nan = np.nan * np.ones((A.shape[0], A.shape[0]))
+        u_vec_nan =  np.nan * np.ones((1, A.shape[0]))
+        return np.nan, wopt_nan, evals_nan, evecs_nan, np.nan, u_vec_nan
+
+    dp2 = (numerator / denominator).squeeze()
+
+    evals, evecs = np.linalg.eig(usig)
+    # make sure evals / evecs are sorted
+    idx_sort = np.argsort(evals)[::-1]
+    evals = evals[idx_sort]
+    evecs = evecs[:, idx_sort]
+
+    # best decoding axis ignoring correlations (reduces to direction of u_vec)
+    wopt_diag = np.linalg.inv(usig_diag) @ u_vec.T
+
+    return dp2, wopt_diag, evals, evecs, evec_sim, u_vec
