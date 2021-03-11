f26a572bb64531f745903aae994c417fe63d55c1
crheller <charlieheller95@gmail.com>
2021-03-10 17:41:38 -0800

    work on tweaks to figure2

--- FigureScripts/fig2.py
+++ FigureScripts/fig2.py
@@ -14,7 +14,7 @@ mpl.rcParams['font.size'] = 8
 
 np.random.seed(123)
 
-savefig = True
+savefig = False
 fig_name = os.path.join(os.getcwd(), 'figures/fig2.svg')
 
 # Generate high-D data with random noise structure
@@ -60,6 +60,11 @@ idx = np.argsort(evals2)[::-1]
 evals2 = evals2[idx]
 evecs2 = evecs2[:, idx]
 
+# decay in covariance with samples depends on the covariance value (and indep. var)
+# so, for sake of comparison, find an index that matches across all three datasets
+cidx = tuple(np.argwhere(abs(cov-0.04) == np.min(abs(cov-0.04)))[0])
+c1idx = tuple(np.argwhere(abs(cov1-0.04) == np.min(abs(cov1-0.04)))[0])
+c2idx = tuple(np.argwhere(abs(cov2-0.04) == np.min(abs(cov2-0.04)))[0])
 # get goodness of fit for eigenvector / a single covariance value
 # across sample sizes. For each sample size, draw nSamples to get 
 # sense of the variance in the estimate for each k
@@ -67,6 +72,8 @@ e1_sim = []
 e1_sim1 = []
 e1_sim2 = []
 cov_val = []
+cov_val1 = []
+cov_val2 = []
 nSamples = 100
 krange = np.arange(10, 150, 2)
 for ii, k in enumerate(krange):
@@ -75,17 +82,21 @@ for ii, k in enumerate(krange):
     _e11 = []
     _e12 = []
     _cov = []
+    _cov1  = []
+    _cov2 = []
     for i in range(nSamples):
         x = np.random.multivariate_normal(u, cov, k)
-        _cov.append(np.cov(x)[0, 1])
+        _cov.append(np.cov(x.T)[cidx])
         _evals, _evecs = np.linalg.eig(np.cov(x.T))
         _e1.append(abs(_evecs[:, np.argmax(_evals)].dot(evecs[:, 0])))
 
         x1 = np.random.multivariate_normal(u, cov1, k)
+        _cov1.append(np.cov(x1.T)[c1idx])
         _evals, _evecs = np.linalg.eig(np.cov(x1.T))
         _e11.append(abs(_evecs[:, np.argmax(_evals)].dot(evecs1[:, 0])))
 
         x2 = np.random.multivariate_normal(u, cov2, k)
+        _cov2.append(np.cov(x2.T)[c2idx])
         _evals, _evecs = np.linalg.eig(np.cov(x2.T))
         _e12.append(abs(_evecs[:, np.argmax(_evals)].dot(evecs2[:, 0])))
 
@@ -93,14 +104,18 @@ for ii, k in enumerate(krange):
     e1_sim1.append(_e11)
     e1_sim2.append(_e12)
     cov_val.append(_cov)
+    cov_val1.append(_cov1)
+    cov_val2.append(_cov2)
 
 e1_sim = np.stack(e1_sim)
 e1_sim1 = np.stack(e1_sim1)
 e1_sim2 = np.stack(e1_sim2)
 cov_val = np.stack(cov_val)
+cov_val1 = np.stack(cov_val1)
+cov_val2 = np.stack(cov_val2)
 
 # Make figure
-f, ax = plt.subplots(1, 3, figsize=(7.2, 2.1))
+f, ax = plt.subplots(1, 4, figsize=(7.2, 1.75))
 
 im = ax[0].imshow(cov2, aspect='auto', cmap='bwr', vmin=-1, vmax=1)
 [s.set_visible(False) for s in ax[0].spines.values()]
@@ -112,30 +127,38 @@ ax[0].set_ylabel("Neuron")
 f.colorbar(im, ax=ax[0])
 
 # scree plot for each dataset
-cmap = cm.get_cmap('Greens', 40)
-ax[1].plot(evals2 / sum(evals2), '.-', markersize=2, lw=1, color=cmap(10))
-ax[1].plot(evals1 / sum(evals2), '.-', markersize=2, lw=1, color=cmap(20))
-ax[1].plot(evals / sum(evals), '.-', markersize=2, lw=1, color=cmap(30))
+cmap = cm.get_cmap('Greens_r', 100)
+ax[1].plot(evals2 / sum(evals2), '.-', markersize=3, lw=1, color=cmap(10))
+ax[1].plot(evals1 / sum(evals2), '.-', markersize=3, lw=1, color=cmap(30))
+ax[1].plot(evals / sum(evals), '.-', markersize=3, lw=1, color=cmap(60))
 ax[1].set_ylabel(r"Fraction var. exp.")
-ax[1].set_xlabel(r"Principal Component")
+ax[1].set_xlabel(r"Principal Component ($\mathbf{e}_1$ - $\mathbf{e}_N$)")
 
-# ariance of cov[0, 1], evec similarity (on twinx)
-ax[2].plot(krange, cov_val.mean(axis=-1))
-ax[2].axhline(cov[0, 1], linestyle='--', color='tab:blue')
-ax[2].set_ylabel(r"Sample $\Sigma_{0, 1}$", color='tab:blue')
+ax[2].plot(krange, e1_sim2.mean(axis=-1), color=cmap(10))
+ax[2].fill_between(krange, e1_sim2.mean(axis=-1)-e1_sim2.std(axis=-1) / np.sqrt(nSamples),
+                            e1_sim2.mean(axis=-1)+e1_sim2.std(axis=-1) / np.sqrt(nSamples), color=cmap(10), alpha=0.5, lw=0)
+ax[2].plot(krange, e1_sim1.mean(axis=-1), color=cmap(30))
+ax[2].fill_between(krange, e1_sim1.mean(axis=-1)-e1_sim1.std(axis=-1) / np.sqrt(nSamples),
+                            e1_sim1.mean(axis=-1)+e1_sim1.std(axis=-1) / np.sqrt(nSamples), color=cmap(30), alpha=0.5, lw=0)
+ax[2].plot(krange, e1_sim.mean(axis=-1), color=cmap(60))
+ax[2].fill_between(krange, e1_sim.mean(axis=-1)-e1_sim.std(axis=-1) / np.sqrt(nSamples),
+                            e1_sim.mean(axis=-1)+e1_sim.std(axis=-1) / np.sqrt(nSamples), color=cmap(60), alpha=0.5, lw=0)
+ax[2].set_ylabel("Cosine similarity\n"+r"(True $\mathbf{e}_1$ vs. sampled)")
 ax[2].set_xlabel(r"Sample size ($k$)")
-ax[2].tick_params(axis='y', labelcolor='tab:blue')
-ax[2].set_ylim((-0.3, 0.3))
+ax[2].axhline(1, linestyle='--', color='k')
+ax[2].set_ylim((0, 1.05))
 
-ax2 = ax[2].twinx()
-ax2.plot(krange, e1_sim2.mean(axis=-1), color=cmap(10))
-ax2.plot(krange, e1_sim1.mean(axis=-1), color=cmap(20))
-ax2.plot(krange, e1_sim.mean(axis=-1), color=cmap(30))
-ax2.spines['right'].set_visible(True)
-ax2.set_ylabel("Cosine similarity\n(True $e_1$ vs. sampled)", color='forestgreen')
-ax2.axhline(1, linestyle='--', color='forestgreen')
-ax2.tick_params(axis='y', labelcolor='forestgreen')
-ax2.set_ylim((0, 1.05))
+# variance of cov[0, 1], evec similarity (on twinx)
+ax[3].plot(krange, (cov_val2.var(axis=-1) / 0.04) * 100, color=cmap(10))
+ax[3].plot(krange, (cov_val1.var(axis=-1) / 0.04) * 100, color=cmap(30))
+ax[3].plot(krange, (cov_val.var(axis=-1) / 0.04) * 100, color=cmap(60))
+ax[3].axhline(0, linestyle='--', color='k')
+ax[3].set_ylabel(r"Percent error in $\Sigma$"+"\n"+r"$Var(\Sigma_{0, 1}) / \Sigma_{0, 1}$")
+ax[3].set_xlabel(r"Sample size ($k$)")
+ax[3].set_ylim((-25, None))
+ax[3].set_yticks(np.arange(0, 400, 100))
+ax[3].grid('both', alpha=0.5)
+
 f.tight_layout()
 
 if savefig:
