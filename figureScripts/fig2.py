9d7136573b3ebb9136687f9232e55ef0f3cd400b
Charlie Heller <charlieheller95@gmail.com>
2022-05-14 13:19:49 +0200

    renaming figures to offset for new figure 1

--- figureScripts/fig2.py
+++ figureScripts/fig2.py
@@ -1,15 +1,18 @@
 """
-Simualtion of e1 estimation vs. individual covariance element estimation
-for a N=100 neuron population with low-D noise structure.
+Simple two-neuron schematic of single trial responses and decoding axis for high / low rep conditions
 
-Compare independent noise to 1-D noise to 1/n noise -- extreme, extreme, real-ish?
+Shuffled distribution of pairwise covariance for the two neurons in each conditions
+
+Covariance matrix
+
+cross-validated d'^2 vs. k and var(covariance) vs. k. On same axes.
 """
-import dDR.utils.surrogate_helpers as sh
+from dDR.utils.decoding import compute_dprime
+from dDR.utils.plotting import compute_ellipse
 
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import cm
 import matplotlib as mpl
 mpl.rcParams['axes.spines.right'] = False
 mpl.rcParams['axes.spines.top'] = False
@@ -17,154 +20,153 @@ mpl.rcParams['font.size'] = 8
 
 np.random.seed(123)
 
-savefig = False
+savefig = True
 fig_name = os.path.join(os.getcwd(), 'figures/fig2.svg')
 
-# Generate high-D data with variable noise structure
-nUnits = 100
-u = np.zeros(nUnits)
+# generate pseudo data, X, for decoding
+k1 = 10
+k2 = 100
+u1 = [2, 0]
+u2 = [-2, 0]
+cov = np.array([[1, 0.4], [0.4, 1]])
+x11 = np.random.multivariate_normal(u1, cov, k1)
+x21 = np.random.multivariate_normal(u2, cov, k1)
+X1 = np.stack([x11, x21])
+x12 = np.random.multivariate_normal(u1, cov, k2)
+x22 = np.random.multivariate_normal(u2, cov, k2)
+X2 = np.stack([x12, x22])
 
-# generate full rank set of eigenvectors
-evecsa = np.concatenate([sh.generate_lv_loading(nUnits, mean_loading=0.4, variance=1, mag=1) for i in range(nUnits)], axis=1)
-evecsa = sh.orthonormal(evecsa)
-evecsa *= 10
+# get decoding axis and projections
+d = compute_dprime(x11.T, x21.T)
+wopt1 = d.wopt * 2
+d = compute_dprime(x12.T, x22.T)
+wopt2 = d.wopt * 2
 
-# DATASET 1: independent noise (diag cov matrix)
-svs = np.append(1, 0.3 / np.arange(2, nUnits+1)**(1/2))
-svs = (svs / sum(svs)) * 10
-cov = np.zeros(evecsa.shape)
-np.fill_diagonal(cov, np.diag(sh.generate_full_rank_cov(evecsa * svs)))
-# Get eigenvalues / eigenvectors of the covariance matrix and sort
-evals, evecs = np.linalg.eig(cov)
-idx = np.argsort(evals)[::-1]
-evals = evals[idx]
-evecs = evecs[:, idx]
+# generate list of shuffled covariance values for k=k1/k2
+nshuff = 500
+scov1 = []
+scov2 = []
+for i in range(0, nshuff):
+    x = np.random.choice(x11[:, 0], x11.shape[0], replace=False)
+    y = np.random.choice(x11[:, 1], x11.shape[0], replace=False)
+    scov1.append(np.cov(x, y)[0, 1])
+    x = np.random.choice(x12[:, 0], x12.shape[0], replace=False)
+    y = np.random.choice(x12[:, 1], x12.shape[0], replace=False)
+    scov2.append(np.cov(x, y)[0, 1])
 
-# DATASET 2: 1-D noise, not aligned (one big dimension, all others small)
-svs = np.append(1, 0.3 / np.arange(2, nUnits+1)**(1/2))
-svs = (svs / sum(svs)) * 10
-cov1 = sh.generate_full_rank_cov(evecsa * svs)
-# Get eigenvalues / eigenvectors of the covariance matrix and sort
-evals1, evecs1 = np.linalg.eig(cov1)
-idx = np.argsort(evals1)[::-1]
-evals1 = evals1[idx]
-evecs1 = evecs1[:, idx]
+# for a range of k, generate shuffled distros
+kscov = []
+dp = []
+krange = np.arange(10, 400, 2)
+_x1 = np.random.multivariate_normal(u1, cov, 1000)
+for i, _k in enumerate(krange):
+    _s = []
+    _dp = []
+    print(f"{int((i/len(krange)) * 100)} percent finished generating shuffles")
+    for i in range(0, nshuff):
+        x = np.random.choice(_x1[:, 0], _k, replace=False)
+        y = np.random.choice(_x1[:, 1], _k, replace=False)
+        _s.append(np.cov(x, y)[0, 1])
+        r1 = np.random.multivariate_normal(u1, cov, _k)
+        r2 = np.random.multivariate_normal(u2, cov, _k)
+        # get cv dprime w/ simple 50/50 split of data
+        est = np.random.choice(np.arange(_k), int(_k/2), replace=False)
+        val = np.array(list(set(np.arange(_k)).difference(set(est))))
+        # train decoder
+        resf = compute_dprime(r1[est, :].T, r2[est, :].T)
+        rest = compute_dprime(r1[val, :].T, r2[val, :].T, wopt=resf.wopt)
+        _dp.append(abs(rest.dprimeSquared - resf.dprimeSquared))
+    dp.append(_dp)
+    kscov.append(_s)
+dp = np.stack(dp)
+kscov = np.stack(kscov)
 
-# DATASET 3: 1/n noise
-svs = 1/np.arange(1, nUnits+1)**(1/2)
-svs = (svs / sum(svs)) * 10
-cov2 = sh.generate_full_rank_cov(evecsa * svs)
-# Get eigenvalues / eigenvectors of the covariance matrix and sort
-evals2, evecs2 = np.linalg.eig(cov2)
-idx = np.argsort(evals2)[::-1]
-evals2 = evals2[idx]
-evecs2 = evecs2[:, idx]
+# plot the results
+f, ax = plt.subplots(2, 3, figsize=(6, 4))
 
-# decay in covariance with samples depends on the covariance value (and indep. var)
-# so, for sake of comparison, find an index that matches across all three datasets
-ccexample = 0.04
-cidx = tuple(np.argwhere(abs(cov-ccexample) == np.min(abs(cov-ccexample)))[0])
-c1idx = tuple(np.argwhere(abs(cov1-ccexample) == np.min(abs(cov1-ccexample)))[0])
-cidx = c1idx
-c2idx = tuple(np.argwhere(abs(cov2-ccexample) == np.min(abs(cov2-ccexample)))[0])
-# get goodness of fit for eigenvector / a single covariance value
-# across sample sizes. For each sample size, draw nSamples to get 
-# sense of the variance in the estimate for each k
-e1_sim = []
-e1_sim1 = []
-e1_sim2 = []
-cov_val = []
-cov_val1 = []
-cov_val2 = []
-nSamples = 100
-krange = np.arange(10, 150, 2)
-for ii, k in enumerate(krange):
-    print(f"Iteration {ii}/{len(krange)}")
-    _e1 = []
-    _e11 = []
-    _e12 = []
-    _cov = []
-    _cov1  = []
-    _cov2 = []
-    for i in range(nSamples):
-        x = np.random.multivariate_normal(u, cov, k)
-        _cov.append(np.cov(x.T)[cidx])
-        _evals, _evecs = np.linalg.eig(np.cov(x.T))
-        _e1.append(abs(_evecs[:, np.argmax(_evals)].dot(evecs[:, 0])))
+# plot decoding figure1
+ax[0, 0].scatter(X1[0, :, 0], X1[0, :, 1], s=3, alpha=0.5, lw=0)
+ax[0, 0].scatter(X1[1, :, 0], X1[1, :, 1], s=3, alpha=0.5, lw=0)
+x, y = compute_ellipse(X1[0, :, 0], X1[0, :, 1])
+ax[0, 0].plot(x, y, color='tab:blue', label='Stim. a')
+x, y = compute_ellipse(X1[1, :, 0], X1[1, :, 1])
+ax[0, 0].plot(x, y, color='tab:orange', label='Stim. b')
+# plot decoding axis
+ref = np.array(u1) - ((np.array(u1) - np.array(u2)) / 2)
+wopt1 = (wopt1 / np.linalg.norm(wopt1)) * 2
+ax[0, 0].plot([ref[0]-wopt1[0], ref[0]+wopt1[0]], 
+            [ref[1]-wopt1[1], ref[1]+wopt1[1]], 'k-')
+ax[0, 0].legend(frameon=False)
+ax[0, 0].set_xlabel(r"$n_1$ spike counts")
+ax[0, 0].set_ylabel(r"$n_2$ spike counts")
+ax[0, 0].axis('equal')
+ax[0, 0].set_title(r"$k=%s$"%str(k1))
 
-        x1 = np.random.multivariate_normal(u, cov1, k)
-        _cov1.append(np.cov(x1.T)[c1idx])
-        _evals, _evecs = np.linalg.eig(np.cov(x1.T))
-        _e11.append(abs(_evecs[:, np.argmax(_evals)].dot(evecs1[:, 0])))
+# plot decoding figure2
+ax[0, 1].scatter(X2[0, :, 0], X2[0, :, 1], s=3, alpha=0.5, lw=0)
+ax[0, 1].scatter(X2[1, :, 0], X2[1, :, 1], s=3, alpha=0.5, lw=0)
+x, y = compute_ellipse(X2[0, :, 0], X2[0, :, 1])
+ax[0, 1].plot(x, y, color='tab:blue', label='Stim. a')
+x, y = compute_ellipse(X2[1, :, 0], X2[1, :, 1])
+ax[0, 1].plot(x, y, color='tab:orange', label='Stim. b')
+# plot decoding axis
+ref = np.array(u1) - ((np.array(u1) - np.array(u2)) / 2)
+wopt2 = (wopt2 / np.linalg.norm(wopt2)) * 2
+ax[0, 1].plot([ref[0]-wopt2[0], ref[0]+wopt2[0]], 
+            [ref[1]-wopt2[1], ref[1]+wopt2[1]], 'k-')
+ax[0, 1].legend(frameon=False)
+ax[0, 1].set_xlabel(r"$n_1$ spike counts")
+ax[0, 1].set_ylabel(r"$n_2$ spike counts")
+ax[0, 1].axis('equal')
+ax[0, 1].set_title(r"$k=%s$"%str(k2))
 
-        x2 = np.random.multivariate_normal(u, cov2, k)
-        _cov2.append(np.cov(x2.T)[c2idx])
-        _evals, _evecs = np.linalg.eig(np.cov(x2.T))
-        _e12.append(abs(_evecs[:, np.argmax(_evals)].dot(evecs2[:, 0])))
+# force a/b to share axes
+ax[0, 0].set_xlim((-5, 5)); ax[0, 0].set_ylim((-5, 5))
+ax[0, 1].set_xlim((-5, 5)); ax[0, 1].set_ylim((-5, 5))
 
-    e1_sim.append(_e1)
-    e1_sim1.append(_e11)
-    e1_sim2.append(_e12)
-    cov_val.append(_cov)
-    cov_val1.append(_cov1)
-    cov_val2.append(_cov2)
+# plot covariance matrix
+ax[0, 2].imshow(cov, cmap='Reds', aspect='auto')
+ax[0, 2].set_xticks([0, 1])
+ax[0, 2].set_yticks([0, 1])
+ax[0, 2].set_xticklabels([r"$n_1$", r"$n_2$"])
+ax[0, 2].set_yticklabels([r"$n_1$", r"$n_2$"])
+for c1 in np.arange(cov.shape[0]):
+    for c2 in np.arange(cov.shape[0]):
+        if c1!=c2:
+            c = 'k'
+        else: 
+            c = 'white'
+        ax[0, 2].text(c1, c2, str(cov[c1, c2]), 
+                        ha='center', va='center', color=c)
+ax[0, 2].set_title(r"$\Sigma=\Sigma_a=\Sigma_b$")
 
-e1_sim = np.stack(e1_sim)
-e1_sim1 = np.stack(e1_sim1)
-e1_sim2 = np.stack(e1_sim2)
-cov_val = np.stack(cov_val)
-cov_val1 = np.stack(cov_val1)
-cov_val2 = np.stack(cov_val2)
+# plot shuffled distro for k=k in decoding figure
+llim, hlim = (-2, 2)
+ax[1, 0].hist(scov1, bins=np.arange(llim, hlim, 0.1), histtype='stepfilled', edgecolor='k', alpha=0.5)
+ax[1, 0].axvline(cov[0, 1], color='r')
+ax[1, 0].set_xlim((llim, hlim))
+ax[1, 0].set_title(r"$k=%s, \sigma^2=%s$" % (str(k1), str(round(np.var(scov1), 3))))
+ax[1, 0].set_xlabel(r"$cov(n_1, n_2)$ after shuffling")
 
-# Make figure
-f, ax = plt.subplots(2, 2, figsize=(4.5, 4))
-ax = ax.flatten()
+ax[1, 1].hist(scov2, bins=np.arange(llim, hlim, 0.1), histtype='stepfilled', edgecolor='k', alpha=0.5)
+ax[1, 1].axvline(cov[0, 1], color='r')
+ax[1, 1].set_xlim((llim, hlim))
+ax[1, 1].set_title(r"$k=%s, \sigma^2=%s$" % (str(k2), str(round(np.var(scov2), 3))))
+ax[1, 1].set_xlabel(r"$cov(n_1, n_2)$ after shuffling")
 
-im = ax[0].imshow(cov2, aspect='auto', cmap='bwr', vmin=-1, vmax=1)
-[s.set_visible(False) for s in ax[0].spines.values()]
-[t.set_visible(False) for t in ax[0].get_xticklines()]
-[t.set_visible(False) for t in ax[0].get_yticklines()]
-ax[0].set_title(r"$\Sigma$")
-ax[0].set_xlabel("Neuron")
-ax[0].set_ylabel("Neuron")
-f.colorbar(im, ax=ax[0])
+# plot range of shuffle var for different values of k
+ax[1, 2].plot(krange, kscov.var(axis=-1))
+ax[1, 2].axhline(0, linestyle='--', color='k')
+ax[1, 2].set_ylabel(r"$\sigma^2$ of shuff. distribution", color='tab:blue')
+ax[1, 2].set_xlabel(r"No. of samples ($k$)")
+ax[1, 2].tick_params(axis='y', labelcolor='tab:blue')
 
-# scree plot for each dataset
-cmap = cm.get_cmap('Greens_r', 100)
-c1 = 'tab:blue' #cmap(10)
-c2 = 'tab:orange' #cmap(30)
-c3 = 'tab:green' #cmap(60)
-ax[1].plot(np.arange(1, nUnits+1), evals1 / evals1.sum(), '.-', markersize=10, markerfacecolor='white', lw=1, color=c2, label='1-D')
-ax[1].plot(np.arange(1, nUnits+1), evals / evals.sum(), '.-', markersize=10, markerfacecolor='white', lw=1, color=c3, label='Indep.')
-ax[1].plot(np.arange(1, nUnits+1), evals2 / evals2.sum(), '.-', markersize=10, markerfacecolor='white', lw=1, color=c1, label='1/n')
-ax[1].set_xscale('log')
-ax[1].set_yscale('log')
-ax[1].set_ylabel(r"Fraction noise var. exp.")
-ax[1].set_xlabel(r"Principal Component ($\mathbf{e}_1$ - $\mathbf{e}_N$)")
-ax[1].legend(frameon=False)
-ax[1].set_ylim((None, 1.1))
-
-ax[2].plot(krange, e1_sim2.mean(axis=-1), color=c1)
-ax[2].fill_between(krange, e1_sim2.mean(axis=-1)-e1_sim2.std(axis=-1) / np.sqrt(nSamples),
-                            e1_sim2.mean(axis=-1)+e1_sim2.std(axis=-1) / np.sqrt(nSamples), color=c1, alpha=0.5, lw=0)
-ax[2].plot(krange, e1_sim1.mean(axis=-1), color=c2)
-ax[2].fill_between(krange, e1_sim1.mean(axis=-1)-e1_sim1.std(axis=-1) / np.sqrt(nSamples),
-                            e1_sim1.mean(axis=-1)+e1_sim1.std(axis=-1) / np.sqrt(nSamples), color=c2, alpha=0.5, lw=0)
-ax[2].plot(krange, e1_sim.mean(axis=-1), color=c3)
-ax[2].fill_between(krange, e1_sim.mean(axis=-1)-e1_sim.std(axis=-1) / np.sqrt(nSamples),
-                            e1_sim.mean(axis=-1)+e1_sim.std(axis=-1) / np.sqrt(nSamples), color=c3, alpha=0.5, lw=0)
-ax[2].set_ylabel("Cosine similarity\n"+r"(True $\mathbf{e}_1$ vs. estimated)")
-ax[2].set_xlabel(r"Sample size ($k$)")
-ax[2].axhline(1, linestyle='--', color='k', zorder=-1)
-ax[2].set_ylim((0, 1.05))
-
-# variance of cov[i, j]
-ax[3].plot(krange, cov_val2.var(axis=-1), color=c1)
-ax[3].plot(krange, cov_val1.var(axis=-1), color=c2)
-ax[3].plot(krange, cov_val.var(axis=-1), color=c3)
-ax[3].text(int(len(krange)/2), ax[3].get_ylim()[-1]/2, r"$\Sigma_{i,j}=%s$" % str(ccexample))
-ax[3].set_ylabel(r"$Var(\hat{\Sigma}_{i, j})$")
-ax[3].set_xlabel(r"Sample size ($k$)")
+# generate twin axes for d'^2
+ax2 = ax[1, 2].twinx()
+ax2.spines['right'].set_visible(True)
+ax2.plot(krange, dp.mean(axis=-1), color='orange')
+ax2.set_ylabel(r"Error in $d'^2$ estimate"+"\nabs(Est. - Val.)", color='orange')
+ax2.tick_params(axis='y', labelcolor='orange')
 
 f.tight_layout()
 
